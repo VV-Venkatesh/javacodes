ae75de8264058dfce2b12a4cb91f5ea3c33e9fc3
VASAMSETTI VENKATESH <36559907+srivens-nani@users.noreply.github.com>
2019-12-07 18:56:17 +0530

    Here is a fun game called Guessing game
    How to play:
    After running the code.
    1.Guess and choose the number between 0 and 99(including 99).
    2.If your guess is correct, that's it you win.
    3.If your guess is incorrect,the cycle of guessing continues until u guess the correct one.
    --->Hmmm....Feeling nervous about the game! Don't worry. My hint will help u out.Try it!!

---
+++ GuessingGame.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class GuessingGame{
+public static void main(String[] args) {
+	int num,sysNum;
+	Scanner sc=new Scanner(System.in);
+	Random rand=new Random();
+	sysNum=rand.nextInt(100);
+	System.out.println("***HI THERE WELCOME TO GUESSING GAME***\nEnter a number between 0 and 99..Try to guess the right one!");
+    do{
+    num=sc.nextInt();
+    if(num==sysNum){
+    	System.out.println("YAY!! Correct,You won the game");
+    	break;
+     }
+    else if(num>sysNum)
+    	System.out.println("oops! Select a smaller number");
+    else
+    	System.out.println("opps! Select a greater number");
+   }while(true);
+}
+}
